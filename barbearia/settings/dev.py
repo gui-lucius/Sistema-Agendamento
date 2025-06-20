15017caf49f6728e66183c01a957cc648be80187
Arthur Ribeiro Goncalves <ribeirogon.arthur@gmail.com>
2025-06-20 15:25:17 -0300

    Update dev.py

--- barbearia/settings/dev.py
+++ barbearia/settings/dev.py
@@ -2,7 +2,13 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.15.8", "https://web-production-79ac1.up.railway.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.15.8", "web-production-79ac1.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-79ac1.up.railway.app',
+    'https://www.barbearia-rd.com.br',
+    'https://barbearia-rd.com.br',
+]
 
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
